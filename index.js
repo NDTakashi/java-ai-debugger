65fe939baae1372fdafaf1cdb982b8554028df21
Subhankar Roy <subhankarcr7301@gmail.com>
2026-05-16 16:38:50 +0530

    Fix production CORS

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] },
+  cors: {
+    origin: [
+      "http://localhost:5173",
+      "https://java-ai-debugger-frontendcopy.vercel.app",
+    ],
+    methods: ["GET", "POST"],
+  },
 });
 
 app.use(cors());
